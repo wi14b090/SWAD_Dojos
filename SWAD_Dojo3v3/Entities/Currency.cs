f0401cc4286b78cabceea4b44e83ebd36008779c
wi14b090 <wi14b090@technikum-wien.at>
2016-10-23 00:14:27 +0200

    Added currency combobox (not coupled with datagrid entries)

---
+++ SWAD_Dojo3v3/Entities/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_Dojo3v3.Entities
+{
+    public class Currency
+    {
+        public String name { get; set;}
+        public Double oneUnitInEUR {   get; set;}
+
+        public Currency(String name, Double oneUnitInEUR)
+        {
+            this.name = name;
+            this.oneUnitInEUR = oneUnitInEUR;
+        }
+    }
+}
--- SWAD_Dojo3v3/ViewModel/MainViewModel.cs
+++ SWAD_Dojo3v3/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 using GalaSoft.MvvmLight;
 using SWAD_Dojo3v3.Entities;
+using System;
 using System.Collections.ObjectModel;
 
 namespace SWAD_Dojo3v3.ViewModel
@@ -7,23 +8,37 @@ namespace SWAD_Dojo3v3.ViewModel
     
     public class MainViewModel : ViewModelBase
     {
-        public ObservableCollection<SWPackage> list { get; set; }
+        public ObservableCollection<SWPackage> listItems { get; set; }
+
+        public ObservableCollection<Currency> listCurr { get; set; }
+
+        public String selectedCurr { get; set; }
+
         public MainViewModel DataContext { get; private set; }
+
+        
 
         public MainViewModel()
         {
             //InitializeComponent();
             this.DataContext = this;
-            list = new ObservableCollection<SWPackage>();
+            listItems = new ObservableCollection<SWPackage>();
+            listCurr = new ObservableCollection<Currency>();
             createTestData();
         }
 
         public void createTestData()
         {
-            list.Add(new SWPackage("Word","Officeware",new decimal(149.99),new decimal(65.44),2));
-            list.Add(new SWPackage("Excel", "Officeware", new decimal(169.99), new decimal(75.44), 15));
-            list.Add(new SWPackage("Power Point", "Officeware", new decimal(189.99), new decimal(85.44), 33));
+            listItems.Add(new SWPackage("Word","Officeware",new decimal(149.99),new decimal(65.44),2));
+            listItems.Add(new SWPackage("Excel", "Officeware", new decimal(169.99), new decimal(75.44), 15));
+            listItems.Add(new SWPackage("Power Point", "Officeware", new decimal(189.99), new decimal(85.44), 33));
 
+            listCurr.Add(new Currency("EUR",1));
+            listCurr.Add(new Currency("USD", 1));
+            listCurr.Add(new Currency("GBP", 1));
+            listCurr.Add(new Currency("CAD", 1));
+
+            selectedCurr = "EUR";
         }
     }
 }
